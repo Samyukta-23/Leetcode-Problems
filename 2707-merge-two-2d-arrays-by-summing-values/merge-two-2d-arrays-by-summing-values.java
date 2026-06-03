7558a7735f983ac859fe9a9c9b3e4660f909f194
Samyukta A <asamyukta2006@gmail.com>
2026-06-03 11:02:44 +0530

    Time: 6 ms (10.36%) | Memory: 47.3 MB (9.10%) - LeetSync

---
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            map.put(nums1[i][0],nums1[i][1]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            map.put(nums2[i][0],map.getOrDefault(nums2[i][0],0)+nums2[i][1]);
+        }
+        int[][] ans=new int[map.size()][2];
+        int row=0;
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int key:map.keySet()){
+            list.add(key);
+        }
+        Collections.sort(list);
+        for(int n:list){
+            ans[row][0]=n;
+            ans[row][1]=map.get(n);
+            row++;
+        }
+        return ans;
+    }
+}
