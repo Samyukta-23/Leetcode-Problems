1cb982d354bf840a05d96653f09a8c8c5f491b3e
Samyukta A <asamyukta2006@gmail.com>
2026-06-10 17:59:51 +0530

    Time: 2 ms (12.77%) | Memory: 43.9 MB (24.59%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int myAtoi(String s) {
+        char[] arr=s.toCharArray();
+        int i=0;
+        boolean neg=false;
+        while(i<arr.length && arr[i]==' '){
+            arr[i]='#';
+            i++;
+        }
+        if(i<arr.length && (arr[i]=='-'||arr[i]=='+')){
+            if(arr[i]=='-') neg=true;
+            arr[i]='#';
+            i++;
+        }
+
+        while(i<arr.length && arr[i]=='0'){
+            arr[i]='#';
+            i++;
+        }
+
+        while(i<arr.length && ( arr[i]=='0' || arr[i]=='1' || arr[i]=='2' || arr[i]=='3' || arr[i]=='4' || arr[i]=='5' || arr[i]=='6' || arr[i]=='7' || arr[i]=='8' || arr[i]=='9')){
+            i++;
+        }
+
+        for(int j=i;j<arr.length;j++){
+            arr[j]='#';
+        }
+
+        StringBuilder str=new StringBuilder();
+        for(char ch:arr){
+            if(ch!='#'){
+                str.append(ch);
+            }
+        }
+        if(str.length()==0) return 0;
+        if(str.length()>10){
+            if(neg) return -2147483648;
+            return 2147483647;
+        }
+        long num=Long.parseLong(str.toString());
+        if(neg) num*=-1;
+        if(num>=2147483647) return 2147483647;
+        if(num<=-2147483648) return -2147483648;
+        return (int)num;
+    }
+}
