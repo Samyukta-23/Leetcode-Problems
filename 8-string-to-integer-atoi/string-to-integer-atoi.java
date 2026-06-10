2719adcc4859ccc0225056caa7b276f4c113d0dd
Samyukta A <asamyukta2006@gmail.com>
2026-06-10 18:02:30 +0530

    Time: 2 ms (12.77%) | Memory: 44 MB (12.75%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -18,7 +18,7 @@ class Solution {
             i++;
         }
 
-        while(i<arr.length && ( arr[i]=='0' || arr[i]=='1' || arr[i]=='2' || arr[i]=='3' || arr[i]=='4' || arr[i]=='5' || arr[i]=='6' || arr[i]=='7' || arr[i]=='8' || arr[i]=='9')){
+        while(i<arr.length && ( arr[i]>='0' && arr[i]<='9')){
             i++;
         }
 
