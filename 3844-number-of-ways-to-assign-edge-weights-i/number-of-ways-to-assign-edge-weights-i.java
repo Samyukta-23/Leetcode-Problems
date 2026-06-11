09a3b49a8df1c8a58f46091d54cb50762d123865
Samyukta A <asamyukta2006@gmail.com>
2026-06-11 12:24:49 +0530

    Time: 210 ms (23.81%) | Memory: 304.9 MB (9.52%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.java
@@ -0,0 +1,53 @@
+class Solution {
+    int max_depth=0;
+    long MOD = 1000000007L;
+    public int assignEdgeWeights(int[][] edges) {
+        HashMap<Integer,ArrayList<Integer>> map= new HashMap<>();
+        for(int i=0;i<=edges.length+1;i++){
+            map.put(i,new ArrayList<>());
+        }
+
+        for(int[] arr:edges){
+            map.get(arr[0]).add(arr[1]);
+            map.get(arr[1]).add(arr[0]);
+        }
+        boolean[] vis=new boolean[edges.length+2];
+        vis[1]=true;
+        traversal(1,0,vis,map);
+        // return (int) modPow(2, max_depth-1);
+        return (int) format(2,max_depth-1);
+    }
+
+    void traversal(int node, int depth, boolean[] vis, HashMap<Integer,ArrayList<Integer>> map){
+        vis[node]=true;
+        max_depth=Math.max(max_depth,depth);
+        for(int nei:map.get(node)){
+            if(!vis[nei]){
+                traversal(nei,depth+1,vis,map);
+            }
+        }
+    }
+
+    long format(long base,long pow){
+        long ans=1;
+        for(int i=1;i<=pow;i++){
+            ans*=base;
+            if(ans>=1000000007){
+                ans%=1000000007;
+            }
+        }
+        return ans;
+    }
+
+    long modPow(long base, long exp) {
+        long ans = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                ans = (ans * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return ans;
+    }
+}
