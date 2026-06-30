bb25889680ab9a9b5bc541d2c7bbe65f049ce276
Samyukta A <asamyukta2006@gmail.com>
2026-06-30 10:22:47 +0530

    Time: 8 ms (91.27%) | Memory: 49 MB (59.21%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,16 +1,29 @@
-import java.util.*;
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        List<int[]> merged = new ArrayList<>();
-        for (int[] interval : intervals) {
-            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
-                merged.add(interval);
-            } else {
-                merged.get(merged.size() - 1)[1] =
-                    Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+        Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
+        ArrayList<int[]> list=new ArrayList<>();
+        for(int[] arr:intervals){
+            if(list.size()==0){
+                list.add(arr);
+            }
+            else if(list.get(list.size()-1)[1]<arr[0]){
+                list.add(arr);
+            }
+            else if(list.get(list.size()-1)[1]>=arr[0] && list.get(list.size()-1)[1]>=arr[1]){
+                continue;
+            }
+            else{
+                list.get(list.size()-1)[1]=arr[1];
             }
         }
-        return merged.toArray(new int[merged.size()][]);
+        int ans[][] =new int[list.size()][2];
+        int k=0;
+        for(int[] arr:list){
+            ans[k][0] = list.get(k)[0];
+            ans[k][1]=list.get(k)[1];
+            k++;
+        }
+        return ans;
     }
+    
 }
