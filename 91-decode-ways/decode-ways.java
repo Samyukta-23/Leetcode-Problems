b6d9c8071ad83d1d873cf95b4e90288a4627f6ee
Samyukta A <asamyukta2006@gmail.com>
2026-08-04 22:51:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (63.81%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp=new int[s.length()+1];
+        if(s.charAt(0)!='0')
+        dp[0]=1;
+        else return 0;
+        dp[1]=1;
+        for(int i=2;i<dp.length;i++){
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            int num = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
+            if(num>=10 && num<=26)
+                dp[i]+=dp[i-2];
+        }
+        return dp[s.length()];
+    }
+    
+}
