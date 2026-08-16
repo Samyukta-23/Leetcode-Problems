4ec966c534fbcd49cc9a3c680b6372bccace450b
Samyukta A <asamyukta2006@gmail.com>
2026-08-16 23:49:44 +0530

    Time: 5 ms (66.29%) | Memory: 47.1 MB (71.66%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,22 +1,25 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp=new int[word1.length()][word2.length()];
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
+        int[][] dp=new int[word1.length()+1][word2.length()+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=i;
         }
-        return helper(word1,word2,word1.length()-1,word2.length()-1,dp);
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                char ch1=word1.charAt(i-1),ch2=word2.charAt(j-1);
+                if(ch1==ch2){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
     }
 
-    int helper(String s1,String s2,int i,int j,int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            return helper(s1,s2,i-1,j-1,dp);
-        }
-        int del=1+helper(s1,s2,i-1,j,dp);
-        int ins=1+helper(s1,s2,i,j-1,dp);
-        int rep=1+helper(s1,s2,i-1,j-1,dp);
-        return dp[i][j]=Math.min(del,Math.min(ins,rep));
-    }
+    
 }
