049ef4c3507dedf5a2c2405134f3a2f89d84a7df
Samyukta A <asamyukta2006@gmail.com>
2026-08-19 21:00:41 +0530

    Time: 612 ms (5.14%) | Memory: 42.1 MB (43.69%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.java
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -6,22 +6,21 @@ class Solution {
         for(int i=0;i<n;i++){
             arr[i]=i+1;
         }
-        helper(arr,new ArrayList<>());
-        return ans;
+        return helper(arr,new ArrayList<>());
     }
 
-    void helper(int[] arr,ArrayList<Integer> list){
+    int helper(int[] arr,ArrayList<Integer> list){
         if(list.size()==arr.length){
-            System.out.println(list);
-            ans++;
-            return;
+            return 1;
         }
+        int ans=0;
         for(int i=0;i<arr.length;i++){
             if(list.contains(arr[i])) continue;
             list.add(arr[i]);
             if((list.size())%arr[i]==0 || arr[i]%(list.size())==0)
-            helper(arr,list);
+            ans+= helper(arr,list);
             list.remove(list.size()-1);
         }
+        return ans;
     }
 }
